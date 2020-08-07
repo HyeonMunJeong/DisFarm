ae53fef8775e312fa3378b5dbbadee328c15de7f
wjdgusans1 <wjdgusans1@naver.com>
2020-08-08 02:35:54 +0900

    DisFarm Start

---
+++ src/main/java/com/disfarm/farm/DisFarmApplication.java
@@ -0,0 +1,13 @@
+package com.disfarm.farm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisFarmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisFarmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/disfarm/farm/DisFarmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.disfarm.farm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisFarmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
